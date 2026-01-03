4a35733f022140076d719325b5fa19454bf6b470
Oguz Karaevli <hipoglisemi@192.168.1.19>
2026-01-03 21:33:43 +0300

    fix(scraper): remove unused undetected_chromedriver import in maximum.py

--- src/scrapers/isbankasi/maximum.py
+++ src/scrapers/isbankasi/maximum.py
@@ -39,7 +39,7 @@ if sys.version_info >= (3, 12):
     except ImportError:
         pass
 
-import undetected_chromedriver as uc
+# import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
